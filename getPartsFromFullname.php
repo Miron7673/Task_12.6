6a126ae614c5d6c43619cd4766ec1068d6a67b07
Miroshnichenko A. A <miron7673@gmail.com>
2022-02-19 06:28:23 +0400

    изменение функции объединения массивов

--- getPartsFromFullname.php
+++ getPartsFromFullname.php
@@ -5,12 +5,7 @@ function getPartsFromFullname($fullName)
 {
     $tempArray = array_values(explode(' ', $fullName));
     $keys = ['surename', 'name', 'patronomyc'];
-    $i=0; 
-
-    foreach ($keys as $key) {    
-        $resultArray[$key] = $tempArray[$i];
-        $i++;
-    }
+    $resultArray = array_combine($keys, $tempArray);
 
     return $resultArray;
 }
